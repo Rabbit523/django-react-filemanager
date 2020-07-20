eed482842bb6e5c1877636b0aac3b974f3b1c525
SuperHero <worldhero2018@hotmail.com>
2020-07-20 15:04:47 -0400

    update get started image path

--- src/helpers/ImageGroup.js
+++ src/helpers/ImageGroup.js
@@ -55,7 +55,7 @@ export const imageGroup128 = {
   pdf:
     "https://www.gstatic.com/docs/doclist/images/mediatype/icon_1_pdf_x128.png",
   getting_started:
-    "https://lh3.googleusercontent.com/PVWhwKXiAHXXqZa31tR66CrPkz59fRmhs0HI6vY_BOhL3xYetpXjg334wVl5eKaR_zhf5-QvCq4jMqlqGvRHjbfZiwyQ2a_zwXieOq4qLV_p08mVtsb_GkpQfOSFsWlYEtPdaaDvc9emR84MqeT-XbXlSChFSGzwYwGNg5xzk-iXMuhlDMavD-HiFOvCPRD-0RhYqgsmhqGWvMX7lrmYW0zDbvIhS8uD4_vom_QrMRegpDlshoMs7gg3w8Z32y1pAhMJX2_KUz057HYtp2HpxMjCMgnk9rxFiPgEEkw3Xm_SRkr5cBN0JnR3mBdr425aaTG3ZXUc3UwskcI7oMmk4Msj62D__ZgI7WuX_G2kPM0-K8gG5sX516ETqPXVMHOzU5UtFCDVbzwQGA4N5Sjfwr8YgdxycSe2MNeFtsuqYv0GWF9r83mm5wJrH3I7OYg6R7Nu_q8wxIVgwkficjIg_hMYMuT7Qb2g3nHg7Pj74FSwZoWBKaJG0ZvfkrPONQia0iOsJYt4xy6yAVWUFhLq2QWkxRX--zzIzwzniOUjSASQhht_PvDvZWJ0mhDZWuDpriXq0h9KRRUbGvemX5ouNUUnkqYaIoa2wX0mhUDPGTaAyogTLTGaxj4I7PuGy4VHkB2dKNDYw-2lza9N-shJ2okbS7X_2uxCb2Tev6_RleGGr3liYou9904NVkSB3Wxl-eBh2A=w200-h190-p-k-nu-ft",
+    "https://images2.imgbox.com/ec/55/ZZt0w3pA_o.png",
   unkown:
     "https://drive-thirdparty.googleusercontent.com/128/type/application/x-ms-dos-executable",
 };
