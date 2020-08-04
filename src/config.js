3d16bab2a52a66987f966197f9a02cebf72aca2c
SuperHero <worldhero2018@hotmail.com>
2020-08-05 03:01:15 +0800

    update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export default config = {
+export const config = {
     BASE_URL: "http://127.0.0.1:8000",
     PROD_BASE_URL: "http://18.133.54.86:8000"
 };
--- src/helpers/RestAPI.js
+++ src/helpers/RestAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import config from "../config";
+import { config } from "../config";
 
 export const getFiles = () => {
   return new Promise((resolve, reject) => {
